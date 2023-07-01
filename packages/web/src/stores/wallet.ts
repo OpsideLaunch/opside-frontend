a27908caba3b2dc8825a43a6e209d46299ecb12e
yong <den9yon9@gmail.com>
2023-07-01 15:42:51 +0800

    fix: account change

--- packages/web/src/stores/wallet.ts
+++ packages/web/src/stores/wallet.ts
@@ -136,7 +136,10 @@ export const useWalletStore = defineStore('wallet', {
         userStore.onLogout()
         // router.replace('/auth/login')
         this.ensureWalletConnected(true).then(() => {
-          window.location.reload()
+          console.log('reload..')
+          setTimeout(() => {
+            window.location.reload()
+          }, 2000)
         })
       }
     },
