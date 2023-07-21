12c1e572ddc3ad223ec19caffca2214737f46308
tower1229 <tower1229@foxmail.com>
2023-07-21 14:40:19 +0800

    fix: 🐛 remove bottom2

--- packages/web/src/layouts/default/blocks/TheFooter/index.tsx
+++ packages/web/src/layouts/default/blocks/TheFooter/index.tsx
@@ -8,14 +8,11 @@ export default defineComponent({
     }
   },
   render() {
-    return null
-    // (
-    // <div class="border-color-border border-t-1 mt-10 text-center pt-4 pb-6 text-color2">
-    //   Powered by
-    //   <a class="text-primary px-1" href="https://twitter.com/WEconomyNetwork" target="_blank">
-    //     @WEconomy
-    //   </a>
-    // </div>
-    // )
+    ;<div class="border-color-border mt-10 text-center pt-4 pb-6 text-color2">
+      {/* Powered by
+      <a class="text-primary px-1" href="https://twitter.com/WEconomyNetwork" target="_blank">
+        @WEconomy
+      </a> */}
+    </div>
   }
 })
